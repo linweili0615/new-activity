9ba44ea415efc3d3a72343f2e73d3b0ca7481f19
linweili <295287765@qq.com>
2019-01-24 19:00:48 +0800

    Initial commit

---
+++ src/main/java/com/activity/test/NewActivityApplication.java
@@ -0,0 +1,14 @@
+package com.activity.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewActivityApplication.class, args);
+    }
+
+}
+
